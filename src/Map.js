5f681503f70a106eaeb48069f64bac17da9b80c7
Nicolas <nchlsreimer@gmail.com>
2022-06-10 16:59:08 -0700

    nick gettin up to date on git

--- src/Map.js
+++ src/Map.js
@@ -1,47 +1,54 @@
 import React from "react";
-import { MapContainer, TileLayer, MapConsumer, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  MapConsumer,
+  useMapEvents,
+} from "react-leaflet";
 // example from sandbox (non-react)
 //import RoutineMachine from "./RoutineMachine";
 import L from "leaflet";
 import icon from "./marker";
-// our react component  
+// our react component
 import Routing from "./RoutingTest";
 
 const Map = (props) => {
-  const [latLang, setLatLang] = React.useState([L.latLng(49.2810, -123.1350),L.latLng(49.2850, -123.1310)])
+  const [latLang, setLatLang] = React.useState([
+    L.latLng(49.281, -123.135),
+    L.latLng(49.285, -123.131),
+  ]);
   const instance = {
     waypoints: latLang,
     lineOptions: {
-      styles: [{ color: "#6FA1EC", weight: 4 }]
+      styles: [{ color: "#6FA1EC", weight: 4 }],
     },
     // shows directions
     show: true,
     // adds way points by dragging
-    addWaypoints: true,
+    addWaypoints: false,
     routeWhileDragging: true,
     // move waypoints by dragging
     draggableWaypoints: true,
     fitSelectedRoutes: true,
     showAlternatives: false,
-  }
-  
+  };
+
   // MyComponent is a method from within react-leaflet, that is the library for react-leaflet hooks
   function MyComponent() {
-    
     // useMapEvents is a React Leaflet Hook
     const map = useMapEvents({
       // var map reprsesents the event listener that create a point when a user clicks on the map
       // on-click event to save lat + lng
       click: (e) => {
         const { lat, lng } = e.latlng;
-        // console.log("🎲 ~ e.latlng", e.latlng);
-        
+        console.log("🎲 ~ e.latlng", e.latlng);
+
         // uses previous state and updates with new state
-        setLatLang(prev => [...prev, L.latLng(lat, lng)])
-  
+        setLatLang((prev) => [...prev, L.latLng(lat, lng)]);
+
         // adds marker to map according to lat, lng
-        L.marker([lat, lng], { icon }).addTo(map);
-      }
+        //L.marker([lat, lng], { icon }).addTo(map);
+      },
     });
     return null;
   }
@@ -50,24 +57,14 @@ const Map = (props) => {
       doubleClickZoom={false}
       id="mapId"
       zoom={14}
-      center={[49.2810, -123.1350]}
+      center={[49.281, -123.135]}
     >
-    <MyComponent/>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=JHPAACJynf7oMojiymA4"
-        />
+      <MyComponent />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=JHPAACJynf7oMojiymA4"
+      />
       <Routing instance={instance} />
-      <MapConsumer>
-        {(map) => {
-          console.log("map center:", map.getCenter());
-          map.on("click", function (e) {
-            const { lat, lng } = e.latlng;
-            L.marker([lat, lng], { icon }).addTo(map);
-          });
-          return null;
-        }}
-      </MapConsumer>
     </MapContainer>
   );
 };

57671e63ff08c431acbc33cfec99e28f36be9eb0
Nicolas <nchlsreimer@gmail.com>
2022-06-12 13:22:57 -0700

    refactored SaveDrawing function to use async await, everything working properly now, including error handling, css code added to hide map directions, new component for burger drop down created and initial styling was started

--- src/Map.js
+++ src/Map.js
@@ -11,9 +11,13 @@ import "./styles.css";
 
 // COMPONENTS FROM OUR APP:
 import Routing from "./Router";
+import DropDownMenu from "./components/DropDownMenu";
 import DeletePointButton from "./components/DeletePointButton";
 import SaveDrawingButton from "./components/SaveDrawingButton";
 import axios from "axios";
+
+// MUI LIBRARY
+import { StyledEngineProvider } from "@mui/material/styles";
 
 //-----------------------------------------------------------------------------------------------------
 // MAP COMPONENT:
@@ -53,16 +57,13 @@ const Map = (props) => {
   // references our .env file
   const api = process.env.REACT_APP_API;
 
-  //const testObject = { message: "poop" };
-
-  // this has been proven to work, sends latLong to express server when called
-  const saveDrawing = () => {
-    return axios.post(`${api}drawings`, latLong)
-      .then((response) => {
-      console.log("Back from saving the drawing", response);
-        //BRING BACK LATER: 
-        setLatLong(); // clear of the current points from the map
-    });
+  const saveDrawing = async () => {
+    try {
+      await axios.post(`${api}drawings`, latLong);
+    } catch (e) {
+      return console.log(e);
+    }
+    setLatLong([]);
   };
 
   //----------------------------------------------------------------------------------------------------
@@ -105,9 +106,15 @@ const Map = (props) => {
         zoom={14}
         center={[49.281, -123.135]}
       >
-        <Control prepend position="bottomleft">
-          {/* <img id="logo" src="Canvas_Logo.png" width="200" height="300"></img> */}
+        {/* <Control prepend position="topleft">
+          <img id="logo" src="Canvas_Logo.png" width="200" height="300"></img>
+        </Control> */}
 
+        <Control>
+          <DropDownMenu></DropDownMenu>
+        </Control>
+
+        <Control prepend position="bottomleft">
           <DeletePointButton removeLastPoint={removeLastPoint}>
             Delete a Point
           </DeletePointButton>
---
+++ src/components/DropDownMenu.jsx
@@ -0,0 +1,89 @@
+// DropDownMenu COMPONENT:
+//----------------------------------------------------------------
+import * as React from "react";
+
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+import GestureIcon from "@mui/icons-material/Gesture";
+import LoginIcon from "@mui/icons-material/Login";
+
+//----------------------------------------------------------------
+
+export default function DropDownMenu(props) {
+  // console.log("🎲 ~ props", props);
+
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Login"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <LoginIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["Drawing Library"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <GestureIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      {["RIGHT"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}

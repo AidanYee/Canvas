fbcb23115e436896dbe9c5a33237ebe46f4dd77c
AidanYee <aidan.yee@pm.me>
2022-06-13 15:56:30 -0700

    add styling sheet

--- src/components/ShowcaseItem.jsx
+++ src/components/ShowcaseItem.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
+import "./ShowcaseItem.scss";
 
 //-------------------------------------------------------------------------------------------
 // SHOWCASE ITEM COMPONENT:

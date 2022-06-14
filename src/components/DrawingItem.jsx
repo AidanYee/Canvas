08b42f11a01b835821a9a36d278523e0943a74cc
AidanYee <aidan.yee@pm.me>
2022-06-13 18:35:14 -0700

    create DrawingItem component

--- src/components/DrawingItem.jsx
+++ src/components/DrawingItem.jsx
@@ -1,7 +1,19 @@
-// import { Button } from "material-ui";
+// DRAWING ITEM COMPONENT:
 
-// const DrawingItem = () => {
-//   return <Button size="small">Drawing</Button>;
-// };
+import Button from "@mui/material/Button";
+//----------------------------------------------
+// -many drawingItem components are rendered in the DropDownMenu component
 
-// export default DrawingItem;
+export default function DrawingItem(props) {
+
+  function renderDrawingPoints() {
+    return props.setLatLong(props.points);
+  }
+
+  return (
+    <Button onClick={renderDrawingPoints} size="small">
+      {props.name}
+    </Button>
+  );
+}
+

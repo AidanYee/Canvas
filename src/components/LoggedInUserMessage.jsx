0b664b1cd697affe5d28904801ae9a9292c8394f
Mitchell Schofield <scho.mitch@gmail.com>
2022-06-14 10:37:24 -0700

    changed props in user message component to display logged in user name as part of message

--- src/components/LoggedInUserMessage.jsx
+++ src/components/LoggedInUserMessage.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Button } from "@mui/material";
 
 const LoggedInUserMessage = (props) => {
+  //This component renders the logged in users name when state is set 
   console.log("logged in user props", props);
   const logout = () => {
     props.setLoggedOut();
   };
+
   return (
     <>
-      <Button variant="text">Welcome Name</Button>
+      <Button variant="text">G Day! {props.name}</Button>
       <Button onClick={logout} variant="text">
         Logout
       </Button>

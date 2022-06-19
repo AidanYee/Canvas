b9fd9d52458c5a36b4e4237ff68dfdd06e7f05c9
Nicolas <nchlsreimer@gmail.com>
2022-06-19 16:56:10 -0700

    fixed showcase bug

--- src/components/ShowcaseItem.jsx
+++ src/components/ShowcaseItem.jsx
@@ -29,7 +29,7 @@ export default function ShowcaseItem(props) {
     screenshot = "/lighthouse_showcase_image.png";
   } else if (props.name === "orca stra") {
     screenshot = "/orca-stra.png";
-  } else if (props.name === "Im Stoked!") {
+  } else if (props.name === " IM STOKED") {
     screenshot = "/im-stoked.png";
   } else if (props.name === " Guy with Hair") {
     screenshot = "/guy_with_hair.png";
